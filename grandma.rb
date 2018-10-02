6b4b8599767bcae5faffc3b0469a6a760c6f3746
John Sime <johnc.sime@gmail.com>
2018-10-02 00:07:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase.equals?("I LOVE YOU GRANDMA!")
+  if phrase.equal?("I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
