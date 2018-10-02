51d12b71c54311b080db8eb603193260a82b99a7
John Sime <johnc.sime@gmail.com>
2018-10-02 00:04:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.downcase
-  puts "HUH?! SPEAK UP, SONNY!"
-else
-  puts "NO, NOT SINCE 1938!"
-end
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.downcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "NO, NOT SINCE 1938!"
+  end
 end
