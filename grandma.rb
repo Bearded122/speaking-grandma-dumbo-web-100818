a7b0f3fb0bdd4d4794f532be3fd5307fb2e2a13b
John Sime <johnc.sime@gmail.com>
2018-10-02 00:10:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
-
+    puts "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
